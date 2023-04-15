c3f64ec6d69c4229428ea2678113eedeac97654e
paul <>
2023-04-15 12:40:15 +0200

    added notice of archival in favor of relaunch of comtradr

--- R/process_comtrade_response.R
+++ R/process_comtrade_response.R
@@ -11,12 +11,12 @@ process_comtrade_response <- function(resp, verbose = F) {
     httr2::resp_body_json(simplifyVector = T)
 
   if (length(result$data) > 0) {
-    if(nrow(result)>249999){
+    if(nrow(result$data)>249999){
         rlang::abort('Your request returns more than 250k rows. This means that most likely not all the data you queried has been returned, as the upper limit is 250k. Please partition your API call, e.g. by using only half the period in the first call.')
-    } else if(nrow(result)>200000){
+    } else if(nrow(result$data)>200000){
         cli::cli_inform(c("i" = "Your request has passed 200k rows. If you exceed 250k rows Comtrade will not return all data. You will have to slice your request in smaller parts."))
     }
-    return(result)
+    return(result$data)
   } else {
     return(data.frame(count = 0))
   }
